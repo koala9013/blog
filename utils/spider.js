72728c798267c94b453988e5ba21b56328eb8c3d
koala9013 <lfk9013@gmail.com>
2015-12-23 15:45:45 +0800

    add underscore
    add underscore & mongoose

--- utils/spider.js
+++ utils/spider.js
@@ -2,30 +2,20 @@ var http = require('http')
 var request = require('request')
 var $ = require('jQuery')
 var fs = require('fs')
-
-// http.createServer(function(req, rsp){
-// 	var url = 'http://www.dbmeinv.com/dbgroup/rank.htm?pager_offset=';
-
-// 	var j = 1000
-// 	while(j > 0){
-// 		spider(url + j);
-// 		j--
-// 	}
-
-// 	rsp.writeHead(200, { 'Content-Type': 'text-plain' });
-// 	rsp.end('sucess');
-// }).listen(3000);
+var _ = require('underscore')
 
 var url = 'http://www.dbmeinv.com/dbgroup/rank.htm?pager_offset=';
 
-// var j = 1000
+var j = 16;
+console.log(j);
 // while(j > 0){
-// 	spider(url + j);
+// 	// process.nextTick(function(){
+// 	// 	spider(url + j);
+// 	// });
+// 	console.log(j);
 // 	j--
 // }
-spider(url + '30');
-console.log('start');
-
+spider(url + j);
 function spider(url){
 	request(url, function(err, response, body){
 		console.log(url + ' response ' + response.statusCode);
@@ -35,15 +25,16 @@ function spider(url){
 
 			if (images.length > 0) {
 				var i = 0
-				for(; i < images.length; i++){
-					var imageURL = $(images[i]).attr('src')
+				//underscore each方法
+				_.each(images, function(element, index){
+					var imageURL = $(element).attr('src')
 					console.log(imageURL);
 					var name = imageURL.substring(30);
 					var path = 'E:\\spider\\' + name;
 
 					//下载文件
 					request(imageURL).pipe(fs.createWriteStream(path));
-				}
+				})
 			}
 		}
 	})
